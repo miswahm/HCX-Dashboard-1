7cbdf9f89231847e2f7f2c2c308c30df6098a1a7
miswah <miswah.md@gmail.com>
2023-03-08 12:48:54 +0530

    (fix): routing for redirection fixed

--- src/app/views/claims/claims.module.ts
+++ src/app/views/claims/claims.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ClaimsComponent } from './claims.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ClaimsComponent } from "./claims.component";
+import { Routes } from "@angular/router";
 
-
+//Routes
+const routes: Routes = [
+  { path: "list", component: ClaimsComponent },
+  { path: "", redirectTo: "/list", pathMatch: "full" },
+  { path: "*", redirectTo: "/list", pathMatch: "full" },
+];
 
 @NgModule({
-  declarations: [
-    ClaimsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ClaimsComponent],
+  imports: [CommonModule],
 })
-export class ClaimsModule { }
+export class ClaimsModule {}
